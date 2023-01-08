b610bae874840df552a2c8c2c1739397779d19a1
Clare Gibson <clarelgibson@gmail.com>
2023-01-08 22:04:47 +0000

    Adds data profiling script initial edit

---
+++ 02-explore/dashProfile.R
@@ -0,0 +1,17 @@
+# Title:        dashProfile
+# Project:      DASH
+# Author:       Clare Gibson
+# Date Created: 2023-01-08
+
+# SUMMARY ######################################################################
+# This script applies data profiling functions to clean data to allow users to
+# see characteristics, summary statistics and relationships for each field in a
+# data set.
+
+# SETUP ########################################################################
+library(dplyr)
+
+# VARIABLES ####################################################################
+
+
+# FUNCTIONS ####################################################################
